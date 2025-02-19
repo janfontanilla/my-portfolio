a08c4bc2449ad09ea0f42bbab6f29e5a5cdd8f9d
Jan <janfontanilla12@gmail.com>
2025-02-19 14:50:53 -0500

    Implement controller, models and routes files

--- src/backend/server.js
+++ src/backend/server.js
@@ -9,6 +9,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to my portfolio application')
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
