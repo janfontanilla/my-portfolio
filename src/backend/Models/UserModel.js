96a976a4f35ad26ee54d5fddb7e4baccf9df6832
janfontanilla <janfontanilla12@gmail.com>
2025-02-26 17:44:07 -0500

    optimize user model schema

--- src/backend/Models/UserModel.js
+++ src/backend/Models/UserModel.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  created: { type: Date, required: true },
-  updated: { type: Date, required: true }
-});
+  password: { type: String, required: true }
+}, { timestamps: true }); 
 
 export default mongoose.model('User', userSchema);
