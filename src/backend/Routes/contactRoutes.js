341ea32a1978f0cf1afe1358d2ed740152bdee13
janfontanilla <janfontanilla12@gmail.com>
2025-02-26 19:23:48 -0500

    fix contact route

--- src/backend/Routes/contactRoutes.js
+++ src/backend/Routes/contactRoutes.js
@@ -1,20 +1,20 @@
 import express from 'express';
-import { 
-  getAllContacts, 
-  getContactById, 
-  createContact, 
-  updateContact, 
-  deleteContact, 
-  deleteAllContacts 
-} from '../Controllers/ContactController.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+  deleteAllContacts
+} from '../Controllers/ContactController.js'; 
 
 const router = express.Router();
 
-router.get('/contacts', getAllContacts);
-router.get('/contacts/:id', getContactById);
-router.post('/contacts', createContact);
-router.put('/contacts/:id', updateContact);
-router.delete('/contacts/:id', deleteContact);
-router.delete('/contacts', deleteAllContacts);
+router.get('/', getAllContacts); 
+router.get('/:id', getContactById);
+router.post('/', createContact); 
+router.put('/:id', updateContact);
+router.delete('/:id', deleteContact);
+router.delete('/', deleteAllContacts); 
 
 export default router;
