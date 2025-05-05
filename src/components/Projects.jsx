6a15f977ce9b98d40ef3cd83a4d09bb695f9ba42
janfontanilla <janfontanilla12@gmail.com>
2025-05-05 12:37:49 -0400

    Update projects and resume section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-//Projects.jsx Jan Fontanilla, 301380907, 2025-01-29
+// Projects.jsx Jan Fontanilla, 301380907, 2025-01-29
 import React from 'react';
-import CalendlyCloneImage from '../assets/images/CalendlyClone.png';
-import CryptoCurrencyImage from '../assets/images/CryptoCurrencyPurchaseAdvisorPlatform.png';
+import VoomImage from '../assets/images/voom.png';
+import EduTutorImage from '../assets/images/EduTutor.png';
 import WeatherAppImage from '../assets/images/WeatherApp.png';
 
 const Projects = () => {
@@ -11,20 +11,20 @@ const Projects = () => {
       <h1>Projects</h1>
       
       <div className="project-item">
-        <h2>Project 1: CalendlyClone</h2>
-        <img src={CalendlyCloneImage} alt="Calendly Clone" width="300" />
-        <p>a clone of Calendly, a popular scheduling app, using Next.js 14, Tailwind CSS, React, and the Nylas API. This project focuses on handling date and time manipulation, and time zone integration. This project focuses on allowing users to schedule meetings, manage their availability while syncrhonizing their calender events. Future improvements will include AI for schedule optimization.</p>
+        <h2>Project 1: Voom</h2>
+        <img src={VoomImage} alt="Voom App" width="250" />
+        <p>Built a full-stack mobile experience using React Native, Expo, TypeScript, and PostgreSQL. Features include onboarding, secure email and OAuth authentication, real-time map-based ride booking with Google Maps and Places Autocomplete, Stripe payment integration, user profile, and trip history. Zustand was used for state management across 15+ modules, with Clerk for authentication. Processed 200+ simulated Stripe transactions with 1.8s average payment latency.</p>
       </div>
 
       <div className="project-item">
-        <h2>Project 2: Cryptocurrency Purchase Advisor Platform</h2>
-        <img src={CryptoCurrencyImage} alt="Crypto Currency Purchase Advisor" width="700" />
-        <p>Work in Progress, Crypto currency blockchain purchase platform with features reccomending users optimal times to purchase units. Uses DexScreener&apos;s API to fetch blockchain cryptocurrency key data such as marketcap, liquidity, and creation date</p>
+        <h2>Project 2: EduTutor</h2>
+        <img src={EduTutorImage} alt="EduTutor Platform" width="500" />
+        <p>Developed a responsive React frontend for an AI-driven document ingestion platform during a remote co-op placement. Implemented interactive UI components to enhance user experience with uploaded documents. Managed application state and integrated real-time API connections to ensure seamless communication between frontend and backend services.</p>
       </div>
 
       <div className="project-item">
         <h2>Project 3: Weather App</h2>
-        <img src={WeatherAppImage} alt="Weather App" width="200" />
+        <img src={WeatherAppImage} alt="Weather App" width="400" />
         <p>Built using HTML, CSS and Javascript, this Weather App allows users to get real-time weather information for any city of their choice. By integrating OpenWeatherMap&apos;s free API, the app fetches and displays key weather data such as temperature, weather conditions, humidity, and wind speed.</p>
       </div>
     </div>
